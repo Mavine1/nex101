63260f7e9bbe6cd66838ba7b3fe9d0b74cbafec8
Mjzo <jmusembi.mukula@gmail.com>
2026-04-05 22:50:01 +0300

    without hit call far rubber

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import { clerkMiddleware } from '@clerk/express';
+import { connectDB } from './config/db.js';
 
 const app = express();
 const port = 4000;
