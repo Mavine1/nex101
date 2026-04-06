e2c846f90ebadd7c01fb54b4b07b61f5c8bbe04d
Mjzo <jmusembi.mukula@gmail.com>
2026-04-06 22:56:01 +0300

    could till burst fruit form

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,7 +7,7 @@ export const connectDB = async () => {
         }
         
         const conn = await mongoose.connect(process.env.MONGODB_URI);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        console.log(`MongoDB Connected`);
     } catch (error) {
         console.error(`Database Error: ${error.message}`);
         process.exit(1);
