d99af31b6bcf019939ac3a06a0074de1c0590947
Mjzo <jmusembi.mukula@gmail.com>
2026-04-06 22:24:01 +0300

    mass ice prove silver earn

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import { connectDB } from './config/db.js';
 import path from 'path';
 import invoiceRouter from './routes/invoiceRouter.js';
 import businessProfileRouter from './routes/businessProfileRouter.js';
+import aiInvoiceRouter from './routes/aiInvoiceRouter.js';
 
 const app = express();
 const port = 4000;
@@ -25,6 +26,7 @@ connectDB();
 // ROUTES
 app.use('/api/invoice', invoiceRouter);
 app.use('/api/businessProfile', businessProfileRouter);
+app.use('/api/ai', aiInvoiceRouter);
 
 app.get('/', (req, res) => {
     res.send('API WORKING');
