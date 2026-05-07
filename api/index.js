3bd06b6372f9a7aaababd43f9a09b71aa753d72b
Mavine <mavinemalin@gmail.com>
2026-05-07 19:10:01 +0300

    steady storm rest thousand up

---
+++ api/index.js
@@ -0,0 +1,6 @@
+// api/index.js
+// Import your main Express app from the backend folder
+import app from '../backend/server.js';
+
+// Export the app as a serverless function for Vercel
+export default app;
