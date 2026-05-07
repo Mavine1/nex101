142a08ce714cfffdaaf6b433ec033637c9e6807d
Mavine <mavinemalin@gmail.com>
2026-05-07 19:34:01 +0300

    smell meal excitement selection anybody

--- api/index.js
+++
@@ -1,6 +0,0 @@
-// api/index.js
-// Import your main Express app from the backend folder
-import app from '../backend/server.js';
-
-// Export the app as a serverless function for Vercel
-export default app;
